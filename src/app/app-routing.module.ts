7541386e18ebb351a3d8439da873c19ee8b7dff6
jannajan <janj58102@gmail.com>
2019-07-07 22:38:26 -0500

    feat: set up initial routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { UsersComponent } from './users/users.component'
+import { DetailsComponent } from './details/details.component'
+import { PostsComponent } from './posts/posts.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  // Specifies the default component that loads on the root path
+  {
+    path: '',
+    component: UsersComponent
+  },
+  // Users detail route, which uses an "id" wildcard.
+  // :id will be fetched from the router
+  {
+    path: 'details/:id',
+    component: DetailsComponent
+  },
+  // Posts route
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
