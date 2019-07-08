9d90d35aa1939f2402e75ca1f31ea9c8ff0b5185
jannajan <janj58102@gmail.com>
2019-07-07 23:12:40 -0500

    feat: get post information from dummy API

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -22,4 +22,8 @@ export class DataService {
   getUser(userId) {
     return this.http.get('https://jsonplaceholder.typicode.com/users/' + userId)
   }
+
+  getPosts() {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts')
+  }
 }
